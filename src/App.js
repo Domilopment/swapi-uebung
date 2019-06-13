3430427cd18a01327bf4ad30bb961849ebbffd2d
Dominic <40559428+Domilopment@users.noreply.github.com>
2019-06-13 17:31:21 +0200

    App and index changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component, Fragment } from 'react'
-import PeopleList from './components/people-list';
+import React, { Component, Fragment } from "react";
+import PeopleList from "./components/people-list";
+import { inject } from "mobx-react";
 
-export default class App extends Component {
+export default
+@inject("peopleStore")
+class App extends Component {
   render() {
+    const people = this.props.peopleStore;
+
+    const handleGetAllPeople = () => {
+      people.getAllPeople();
+    };
+
     return (
       <Fragment>
-        <PeopleList></PeopleList>
+        {handleGetAllPeople()}
+        <PeopleList people={people} />
       </Fragment>
-    )
+    );
   }
 }
